d8b51dff1d1e4750b2c15e797690714d595a525c
Apaark13 <apaark13@gmail.com>
2024-01-04 21:31:34 +0530

    updated home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,47 @@
-
-export default function Home() {
-  function Card(srcp:any) {
-    return(
-
-      <div className="card">
-      <a href="/">
-         <div className="card-thumbnail">
-            <img src='/assets/image.jpeg' loading="lazy" alt="Explore NeoBrutalismCSS" className="card-thumbnail"/>
-          </div>
-          <div className="card-content">
-             <div className="text-small">January 3, 2024</div>
-                    <p>
-                        <strong>Explore ProcessCSS: A Revolutionary library</strong>
-                    </p>
-                    <p>Unveil the future of NeoBrutalism with our groundbreaking approach that will transform your life.</p>
-                    <p><strong>Learn More </strong></p>
-             </div>
-        </a>
-   </div>
-      )
-  }
+function Card({ source }: any) {
   return (
-<div className="flex">
-  
-<Card src="/assets/image.jpeg"/>
-<Card src="/assets/image.jpeg"/>
-<Card src="/assets/image.jpeg"/>
-                  
-</div>
-  )
+    <div className="card">
+      <a href="/">
+        <div className="card-thumbnail">
+          <img
+            src={source}
+            loading="lazy"
+            alt="Explore NeoBrutalismCSS"
+            className="card-thumbnail"
+          />
+        </div>
+        <div className="card-content">
+          <div className="text-small">January 3, 2024</div>
+          <p>
+            <strong>Explore ProcessCSS: A Revolutionary library</strong>
+          </p>
+          <p>
+            Unveil the future of NeoBrutalism with our groundbreaking approach
+            that will transform your life.
+          </p>
+          <p>
+            <strong>Learn More </strong>
+          </p>
+        </div>
+      </a>
+    </div>
+  );
+}
+export default function Home() {
+  return (
+    <>
+      <div className="card nav">
+      <button className="nb-button default rounded p-1 m-1"> Home </button>
+      <button className="nb-button orange rounded p-1 m-1"> Projects </button>
+      <button className="nb-button pale-red rounded p-1 m-1"> About </button>
+      <button className="nb-button green rounded p-1 m-1"> Contact </button>
+      </div>
+
+      <div className="flex">
+        <Card source="/assets/image.jpeg" />
+        <Card source="/assets/image (1).jpeg" />
+        <Card source="/assets/image (2).jpeg" />
+      </div>
+    </>
+  );
 }
