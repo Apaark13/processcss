8fbad43679fba3e8f1a704b7b819df93e933fe9e
Apaark13 <apaark13@gmail.com>
2024-01-05 21:57:10 +0530

    navbar update and added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 function Card({ source }: any) {
   return (
-    <div className="card">
+    <div className="card bg-white">
       <a href="/">
         <div className="card-thumbnail">
           <img
@@ -30,17 +30,23 @@ function Card({ source }: any) {
 export default function Home() {
   return (
     <>
-      <div className="card nav">
-      <button className="nb-button default rounded p-1 m-1"> Home </button>
-      <button className="nb-button orange rounded p-1 m-1"> Projects </button>
-      <button className="nb-button pale-red rounded p-1 m-1"> About </button>
-      <button className="nb-button green rounded p-1 m-1"> Contact </button>
+    <div className="card nav justify-between">
+      <div className="logo flex items-center">
+      <img src="/assets/logo.png" alt="" className="w-20 px-2"/>
+      <h1 className="text-2xl font-bold">ProcessCSS</h1>
       </div>
-
       <div className="flex">
-        <Card source="/assets/image.jpeg" />
-        <Card source="/assets/image (1).jpeg" />
-        <Card source="/assets/image (2).jpeg" />
+        <button className="nb-button default rounded p-1 m-1"> Home </button>
+        <button className="nb-button orange rounded p-1 m-1"> Projects </button>
+        <button className="nb-button pale-red rounded p-1 m-1"> About </button>
+        <button className="nb-button green rounded p-1 m-1"> Contact </button>
+      </div>
+    </div>
+
+      <div className="flex flex-wrap justify-evenly">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Card key={index} source={`/assets/image (${index+1}).jpeg`} />
+        ))}
       </div>
     </>
   );
