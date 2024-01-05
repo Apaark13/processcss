24d9081662f15e6e1ade537a3d32a86fb4f6beff
Apaark13 <apaark13@gmail.com>
2024-01-05 22:36:54 +0530

    routing and button functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './global.scss'
+import Link from 'next/link';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +21,25 @@ export default function RootLayout({
       <head>
       <link rel="stylesheet" href="https://unpkg.com/neobrutalismcss@latest" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="card nav justify-between">
+      <div className="logo flex items-center">
+      <img src="/assets/logo.png" alt="" className="w-20 px-2"/>
+      <h1 className="text-2xl font-bold">ProcessCSS</h1>
+      </div>
+      <div className="flex">
+
+        <button className="nb-button default rounded p-1 m-1"> <Link href="/">
+        Home
+        </Link> </button>
+        <button className="nb-button orange rounded p-1 m-1" > <Link href="/projects">
+        Projects
+        </Link> </button>
+        <button className="nb-button pale-red rounded p-1 m-1"> About </button>
+        <button className="nb-button green rounded p-1 m-1"> Contact </button>
+      </div>
+    </div>
+    {children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 function Card({ source }: any) {
   return (
     <div className="card bg-white">
@@ -30,18 +31,7 @@ function Card({ source }: any) {
 export default function Home() {
   return (
     <>
-    <div className="card nav justify-between">
-      <div className="logo flex items-center">
-      <img src="/assets/logo.png" alt="" className="w-20 px-2"/>
-      <h1 className="text-2xl font-bold">ProcessCSS</h1>
-      </div>
-      <div className="flex">
-        <button className="nb-button default rounded p-1 m-1"> Home </button>
-        <button className="nb-button orange rounded p-1 m-1"> Projects </button>
-        <button className="nb-button pale-red rounded p-1 m-1"> About </button>
-        <button className="nb-button green rounded p-1 m-1"> Contact </button>
-      </div>
-    </div>
+    
 
       <div className="flex flex-wrap justify-evenly">
         {Array.from({ length: 10 }).map((_, index) => (
