6e6e7e821462de3543758efc317c6a29c93befa0
Apaark13 <apaark13@gmail.com>
2024-01-12 23:45:09 +0530

    first project added

---
+++ src/app/projects/project-one/page.tsx
@@ -0,0 +1,16 @@
+import "./project-one.scss"
+const page = () => {
+  return (
+    <div className="main-container">
+    <div className="img-container">
+      <img src="/assets/img (1).jpg" alt="" />
+      <img src="/assets/img (2).jpg" alt="" />
+      <img src="/assets/img (3).jpg" alt="" />
+      <img src="/assets/img (4).jpg" alt="" />
+      <img src="/assets/img (5).jpg" alt="" />
+    </div>
+    </div>
+  );
+};
+
+export default page;
